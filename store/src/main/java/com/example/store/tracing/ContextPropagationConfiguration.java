794d6a1b300398750109eba2c2386bdc030e6669
Laurent Broudoux <laurent.broudoux@gmail.com>
2026-05-28 11:51:07 +0200

    Adding explanation on why RestClientCustomizer no longer work Signed-off-by: Laurent Broudoux <laurent.broudoux@gmail.com>

--- store/src/main/java/com/example/store/tracing/ContextPropagationConfiguration.java
+++ store/src/main/java/com/example/store/tracing/ContextPropagationConfiguration.java
@@ -7,6 +7,7 @@ import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.context.Context;
 
 import org.springframework.ai.mcp.customizer.McpClientCustomizer;
+import org.springframework.boot.restclient.RestClientCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.support.ContextPropagatingTaskDecorator;
@@ -20,6 +21,23 @@ public class ContextPropagationConfiguration {
     @Bean
     ContextPropagatingTaskDecorator contextPropagatingTaskDecorator() {
         return new ContextPropagatingTaskDecorator();
+    }
+
+    /**
+     * Configures a RestClientCustomizer to propagate OpenTelemetry context.
+     * Was working until Spring-AI 2.0.0-M3 but now broken, likely due to changes in Anthropic Java SDK 
+     * that's is now using OkHttp instead of Spring's RestTemplate under the hood. This is a known issue and will be fixed in a future release.
+     */
+    @Bean
+    RestClientCustomizer tracePropagationRestClientCustomizer(OpenTelemetry openTelemetry) {
+        return builder -> builder.requestInterceptor((request, body, execution) -> {
+            openTelemetry.getPropagators().getTextMapPropagator().inject(
+                    Context.current(),
+                    request.getHeaders(),
+                    (headers, key, value) -> headers.set(key, value)
+            );
+            return execution.execute(request, body);
+        });
     }
 
     @Bean
